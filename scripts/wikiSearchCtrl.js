e9a4405b6d0d7c27bad994b819694154360bd896
alvaro911 <algp232@hotmai.com>
2016-05-14 12:34:37 -0600

    Added a factory recipe to resolve API requests

--- scripts/wikiSearchCtrl.js
+++ scripts/wikiSearchCtrl.js
@@ -1,10 +1,28 @@
 angular
 	.module('WikipediaSearch')
-	.controller('wikiSearchCtrl', wikiSearch);
+	.controller('wikiSearchCtrl', wikiSearch)
+	.factory('wikiSearchService', [ '$http', '$q', wikiSearchService]);
 
 	function wikiSearch(){
 		var self = this;
 		this.form = function(){
 			console.log('please work');
+			return false;
 		};
 	} 
+
+	function wikiSearchService($http, $q){
+		var search = function(term){
+			var deferred = $q.defer();
+			$http({
+				method: 'GET',
+				url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&callback=&srsearch=meaning&srprop=snippet|sectiontitle&srlimit=100'
+			}).then(function(result){
+				deferred.resolve(result.search);
+			},function(error){
+				alert(error);
+			});
+			return deferred.promise;
+		};
+		return {search: search};
+	}
--- wiki.js
+++ wiki.js
@@ -1,10 +1,2 @@
-angular.module('WikipediaSearch',[])
-	.controller('wikisearchCtrl', wikiSearch);
-
-function wikiSearch(){
-	var self = this;
-	this.form = function(search){
-		console.log('this is working');
-		return false;
-	};
-}
+angular.module('WikipediaSearch',['$http', '$q']);
+	
